558eee81ce8a8c360cdf562d54fa979d32d164b9
Maxim Bukvich <bukvich.m.ni@sberbank.ru>
2021-03-16 17:43:22 +0300

    Move imageMagicPath to .properties file.

--- src/main/java/com/bukvich/utils/image/converter/Converter.java
+++ src/main/java/com/bukvich/utils/image/converter/Converter.java
@@ -8,16 +8,16 @@ import org.im4java.core.IMOperation;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 @Slf4j
 @RequiredArgsConstructor
 public class Converter {
-    private static final String IMAGE_MAGICK_PATH = "/usr/local/Cellar/imagemagick/7.0.11-3/bin";
-
+    private final String imageMagickPath;
     private final File imageDirectory;
-    private final FileFilter heicFilter = (pathname) -> {
-        return pathname.isFile() && pathname.getName().endsWith(".HEIC");
-    };
+    private final FileFilter heicFilter = (pathname) -> pathname.isFile() && pathname.getName().endsWith(".HEIC");
 
     public void run() {
         File[] files = imageDirectory.listFiles(heicFilter);
@@ -27,7 +27,7 @@ public class Converter {
         }
         log.info("Found {} files in {} directory. Starting conversion...", files.length, imageDirectory.getAbsolutePath());
         ConvertCmd convertCmd = new ConvertCmd();
-        convertCmd.setSearchPath(IMAGE_MAGICK_PATH);
+        convertCmd.setSearchPath(imageMagickPath);
         for (File file : files) {
             log.info("Process {} file.", file.getAbsolutePath());
             IMOperation imOperation = new IMOperation();
@@ -44,12 +44,15 @@ public class Converter {
         log.info("All files have been conversed!");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length < 1) {
             throw new IllegalArgumentException("Please specify path to a folder with images");
         }
         File dir = new File(args[0]);
-        Converter converter = new Converter(dir);
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("app.properties"));
+        String imageMagicPath = properties.getProperty("IMAGE_MAGIC_PATH");
+        Converter converter = new Converter(imageMagicPath, dir);
         converter.run();
     }
 }
